e0d24d880ca183f355fbef8aafb1f540da626993
Nina Kazzim <kazzim@icloud.com>
2019-08-05 23:24:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
 class Owner
-  # code goes here
+  attr_reader :species, :name
+
+  @@owners = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@owners << self
+  end
+  
+  
+  def self.all
+    @@owners
+  end
+
+  def self.count
+    @@owners.size
+  end
+
+  def self.reset_all
+    @@owners.clear
+  end
+  
+  def say_species
+    return "I am a #{@species}."
+  end
+  
+  def cats
+    Cat.each {|cat| cat.owner == self}
+  end
+   
+  def dogs
+    Dog.each {|dog| dog.owner == self}
+  end
+
 end
